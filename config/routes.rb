b945bbcb2704891f951997b6f0552cdf8f83d79c
jparrker <102701917+jparrker@users.noreply.github.com>
2023-07-15 16:18:39 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
+
+
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  delete 'tasks/:id' => 'tasks#destroy'
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put '/tasks/:id/mark_active' => 'task#mark_active'
 end
